a4bda02d58d251ebf2fb09734f6aaada238f0285
iftekharalammithu45 <iftekharalammithu45@gmail.com>
2024-07-16 19:50:27 +0000

    update

--- src/Component/Chart_history.jsx
+++ src/Component/Chart_history.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import { BsCurrencyDollar } from "react-icons/bs";
 import { FaPlusCircle } from "react-icons/fa";
 import { FaMinusCircle } from "react-icons/fa";
@@ -6,11 +7,11 @@ import { MdArrowOutward } from "react-icons/md";
 
 const Chart_history = () => {
   return (
-    <div className="mt-8 gap-2 flex justify-between ">
+    <div className="mt-8 gap-7 custom-border flex justify-between ">
       <div className=" relative h-90 bg-white p-4 rounded-xl flex-1">
         {/* chart Conmonent */}
         <div className=" flex justify-between items-center">
-          <div className="gap-4 flex flex-col">
+          <div className="gap-2 flex flex-col">
             <p>Current Price</p>
             <span className="flex font-[600] text-2xl text-black items-center">
               <BsCurrencyDollar />
@@ -36,7 +37,46 @@ const Chart_history = () => {
           Chart Not Available!
         </div>
       </div>
-      <div className=" h-90 bg-white p-4 rounded-xl flex-1">History</div>
+      <div className=" h-90 bg-white p-4 rounded-xl flex-1">
+        <div className="text-xl">Recent Transaction</div>
+        <div className="flex flex-col  mt-6 gap-4 ">
+          <div className=" px-2  items-center flex justify-between h-14">
+            <div className="flex items-center gap-1">
+              <BsCurrencyDollar className="h-7 w-7" />
+              <div>
+                <p className="text-black text-lg font-[500]">Deposit</p>
+                <span>2024-02-15 14:05:00</span>
+              </div>
+            </div>
+            <div className="text-lg font-medium">+ 81,623</div>
+          </div>
+          <div className=" px-2  items-center flex justify-between h-14">
+            <div className="flex items-center gap-1">
+              <BsCurrencyDollar className="h-7 w-7" />
+              <div>
+                <p className="text-black text-lg font-[500]">Sell</p>
+                <span>2024-02-15 14:05:00</span>
+              </div>
+            </div>
+            <div className="text-lg font-medium">- 5.568457542</div>
+          </div>
+          <div className=" px-2 items-center flex justify-between h-14">
+            <div className="flex items-center gap-1">
+              <BsCurrencyDollar className="h-7 w-7" />
+              <div>
+                <p className="text-black text-lg font-[500]">Withdraw</p>
+                <span>2024-02-15 14:05:00</span>
+              </div>
+            </div>
+            <div className="text-lg font-medium">+ 81,623</div>
+          </div>
+          <div className=" items-center bg-[#C9D6B9] font-medium flex rounded-xl justify-center h-14">
+            <button className="h-full rounded-xl w-full hover:bg-[#afbe8e]">
+              View All
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
